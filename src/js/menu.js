bc428973b07615f58af60f60e20df9af4f84ad9d
MariiaHolovchenko <mgolovchenko268@gmail.com>
2021-06-23 12:06:02 +0300

    правки в мобильное меню

--- src/js/menu.js
+++ src/js/menu.js
@@ -1,9 +1,16 @@
 $(document).ready(function(){
   const burgerRef = document.querySelector("[data-menu-burger]");
   const mobileMenuRef = document.querySelector("[data-menu]");
-
+  const mobileBtnClose = document.querySelectorAll("[data-menu-close]");
+  
   burgerRef.addEventListener("click", () => {
     burgerRef.classList.toggle("is-active");
     mobileMenuRef.classList.toggle("is-open");
+  });
+  mobileBtnClose.forEach(e => {
+    e.addEventListener("click", () => {
+      mobileMenuRef.classList.toggle("is-open");
+      burgerRef.classList.toggle("is-active");
+    });
   });
 });
